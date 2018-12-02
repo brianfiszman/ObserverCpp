03487c60133acc438880ae7c56af8c7973747e1a
Brian Fiszman <fiszmanbrian@gmail.com>
2018-12-02 07:47:15 -0300

    Connections are going smoothly

--- main.cpp
+++ main.cpp
@@ -23,36 +23,48 @@ int chk_argn(int *argc) {
 
 const void listenConnections(ClientCluster *cc, Server *server,
                              fd_set *master) {
-  FD_ZERO(master);
-  FD_SET(server->getListeningFd(), master);
-  select(FD_SETSIZE, master, NULL, NULL, NULL);
+  while (true) {
+    FD_ZERO(master);
+    FD_SET(server->getListeningFd(), master);
+    select(FD_SETSIZE, master, NULL, NULL, NULL);
 
-  if (FD_ISSET(server->getListeningFd(), master)) {
-    lock_guard<std::mutex> guard(myMutex);
-    cc->createClient(server->acceptClient());
+    if (FD_ISSET(server->getListeningFd(), master)) {
+      lock_guard<std::mutex> guard(myMutex);
+      cc->createClient(server->acceptClient());
+    }
   }
-
-  return listenConnections(cc, server, master);
 }
 
-const void start(Server &server) {
-  server.initAndListen();
-  fd_set         master;
-  ClientCluster *cc = server.getClientCluster();
-
-  thread(listenConnections, cc, &server, &master).detach();
-
+const void receiveMessages(ClientCluster *cc, fd_set *reader) {
+  struct timeval selTimeout;
+  selTimeout.tv_sec  = 0; /* timeout (secs.) */
+  selTimeout.tv_usec = 0; /* 0 microseconds */
   do {
+    FD_ZERO(reader);
     if (cc->getClients().size() > 0) {
-      cout << "Hola";
+      for (auto client : cc->getClients()) FD_SET(client.getSockfd(), reader);
+
+      select(FD_SETSIZE, reader, NULL, NULL, &selTimeout);
+      lock_guard<std::mutex> guard(myMutex);
       for (auto client : cc->getClients()) {
-        if (client.receive() <= 0) {
-          cc->destroyClient(client);
-          break;
+        if (FD_ISSET(client.getSockfd(), reader) > 0) {
+          if (client.receive() <= 0) {
+            cc->destroyClient(client);
+            continue;
+          }
         }
       }
     }
   } while (true);
+}
+
+const void start(Server &server) {
+  server.initAndListen();
+  fd_set         master, reader;
+  ClientCluster *cc = server.getClientCluster();
+
+  thread(listenConnections, cc, &server, &master).detach();
+  thread(receiveMessages, cc, &reader).join();
 
   close(server.getListeningFd());
 }

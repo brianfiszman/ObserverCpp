abfd3041bacaa1d511c99fc5504e7e12878a60cf
Brian Fiszman <brianezequielfiszman@gmail.com>
2019-06-04 14:38:32 -0300

    Separated workers into classes

--- workers/workers.hpp
+++ workers/workers.hpp
@@ -7,9 +7,6 @@
 #include <thread>
 #include "../server.hpp"
 
-const bool isAnyoneConnected(ClientCluster *cc);
-const void listenConnections(Server *server, fd_set *listenSet);
-const void receiveMessages(Server *server, fd_set *reader);
-const void start(Server &server);
+const void start(Server &);
 
 #endif
